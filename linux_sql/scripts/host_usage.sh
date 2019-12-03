31170624441f7d0bb5fef4914db3c248088c2cbe
myanneng <contact@anne-nguyen.io>
2019-12-03 12:29:12 +0000

    Edited host_usage.sh to add in password passing.

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -23,11 +23,11 @@ timestamp=$(vmstat -t | awk '{if(NR==3) print $18" "$19}' | xargs)
 # CPU utilization data.
 memory_free=$(echo "$vmstat" | awk '{if(NR==3) print $4}' | xargs)
 cpu_idle=$(echo "$disk" | awk '{if(NR==2) print 100-$5}' | xargs)
-cpu_kernel=(echo "$meminfo" | egrep "^KernelStack:" | awk '{print $2" "$3}' | xargs)
+cpu_kernel=$(echo "$meminfo" | egrep "^KernelStack:" | awk '{print $2}' | xargs)
 disk_io=$(vmstat --unit M | awk '{if(NR==3) print $9}' | xargs)
 disk_available=$(echo "$disk" | awk '{if(NR==3) print $4}' | xargs)
 
-psql -h $hostname -U $psql_user -w $database_name -p $psql_port -c \
+export PGPASSWORD='$psql_password'; psql -h $hostname -U $psql_user -w $database_name -p $psql_port -c \
         "INSERT INTO host_usage
          (hostname, timestamp, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available) 
          VALUES ('"$hostname"', $memory_free, $cpu_idle, $disk_io, $disk_available);"
