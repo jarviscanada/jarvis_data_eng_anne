8bbb34ca9b668eb3a787073c880041c41ab5434f
Anne Nguyen <contact@anne-nguyen.io>
2020-01-27 18:00:44 -0500

    Reuploading grep project files.

---
+++ core_java/grep/README.md
@@ -0,0 +1,46 @@
+# Introduction
+
+`grep` is a Java implementation of the `grep` Bash command-line utility used for searching through plain-text data sets for lines that match a text pattern. The app is given a directory and searches for a text pattern recursively. (This means that the app searches for the text pattern in the files in the deepest subdirectory first, then moves upwards until the algorithm is back in the root/given directory.)
+
+*Fun fact!* `grep` is named in reference to `g/re/p`: **g**lobal search with **r**egular **e**xpression and **p**rinting the lines that match.
+
+# Usage
+
+`grep` takes three arguments.
+1. `regex`: text pattern that lines are matched against
+2. `rootPath`: the path to the root directory where the search takes place
+3. `outFile`: output file name where the results are stored.
+
+For example, given arguments 
+
+```"Anne Nguyen" ~/Documents ~/Documents/results.txt```
+
+`grep` will search through all of the files present in the documents folder, including subdirectories, for "Anne Nguyen." When the app finds a file with a line that contains "Anne Nguyen," it will record that line, and continue its search. Once it completes the search, the app creates a file called `results.txt` in the Documents folder. In `results.txt` is every line that the `grep` app found with "Anne Nguyen" (the given text pattern).
+
+# Pseudocode
+Below is a simplified version of the `process()` code found within this `grep` app.
+
+```
+matchedLines = []
+
+for file in listFilesRecursivelyFrom(rootDirectory)
+    for line in readLines(file)
+        if containsPattern(line)
+            matchedLines.add(line)
+
+writeToFile(matchedLines)
+```
+
+Two nestled loops are required to go through every file and to go through every line of each file. `grep` checks each line to see if it matches the text pattern. If it does, then `grep` adds the line to its record. At the end of the nestled loops, `grep` writes the matched lines into an output file.
+
+# Performance Issue
+
+Given a large enough file, `grep` may not have sufficient memory space to process and store matched lines due to the initial design of using a while-loop to read the file line by line and storing the lines in a `ArrayList<String>`. If there is not enough room on the stack to accomodate every line in the file, lines will be lost while `readLines()` is running. 
+
+To address this perfomance issue, the line collecting in `grep` will use `Stream` instead of `Scanner` contained in a while-loop. `Stream` can break down a data set into piecework, which allows for parallelization. This is in sharp contrast to loops, which work in serial. The switch to `Stream` improves both space and time performance for large file processing by opening the possibility for concurrent processing and ensures no data is lost. 
+
+# Improvements
+
+1. Highlight the part of the line that matches the given `regex` input for every line stored in the output file.
+2. Add filename and line number to the matched line. This is so for every matched line, the user reading the output file knows which file and where in the file every line is.
+3. Proper error log recording. `printStackTrace()` is not an adequate or human-readable solution.
---
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -0,0 +1,165 @@
+package ca.jrvs.apps.grep;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class JavaGrepImp implements JavaGrep {
+
+    private String regex;
+    private String rootPath;
+    private String outFile;
+
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("USAGE: regex rootPath outFile");
+        }
+
+        JavaGrepImp javaGrepImp = new JavaGrepImp();
+        javaGrepImp.setRegex(args[0]);
+        javaGrepImp.setRootPath(args[1]);
+        javaGrepImp.setOutFile(args[2]);
+
+        try{
+            javaGrepImp.process();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Traverses a given directory and returns all files.
+    @Override
+    void process() throws IOException{
+
+        try {
+            // List of files.
+            List<File> fileList = listFiles(rootPath);
+            // List of text lines derived from the list of files.
+            List<String> lines = new ArrayList<>();
+            // Subset of the list of text lines made up of lines that match the regex.
+            List<String> matchedLines = new ArrayList<>();
+
+            // Go through every file and read the lines.
+            // If the line matches the given regex, record the line and write all of the
+            // matched lines to file.
+            for (File file: fileList) {
+                lines = readLines(file);
+                for (String line : lines) {
+                    if (containsPattern(line)) {
+                        matchedLines.add(line);
+                    }
+                }
+            }
+            writeToFile(matchedLines);
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    // List the files in a given directory, including files in subdirectories.
+    @Override
+    List<FIle> listFiles(String rootDir){
+        File root = new File(rootDir);
+        File[] list = root.listFiles();
+        List<File> fileList = new ArrayList<>();
+
+        // If there are no files or subdirectories in the given directory,
+        // there's nothing to list so return null.
+        //if (list == null){
+        //    return null;
+        //}
+
+        try {
+            for (File filename: list) {
+                // To account for files in subdirectories, obtain a list of files in those
+                // subdirectories and then add them to the overarching file list.
+                if (filename.isDirectory()) {
+                    List<File> subdirectoryContents = File.listFiles(filename.getAbsolutePath());
+                    fileList.addAll(subdirectoryContents);
+                }
+                // If the filename isn't a directory, add it to the list.
+                else {
+                    fileList.add(filename);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return fileList;
+    }
+
+    // Read the text from the input file (line by line).
+    @Override
+    List<String> readLines(File inputFile){
+        List<String> listLines = new ArrayList<>();
+
+        try {
+            // Text scanner to read the input file line by line.
+            Scanner scanner = new Scanner(inputFile);
+            // If there are more lines to be read from the file, move onto the next
+            // line and add the line to the list.
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                listLines.add(line);
+            }
+            // Once there are no more lines, close the scanner.
+            scanner.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return listLines;
+    }
+
+    // Given a line, check to see if it matches given regex.
+    @Override
+    boolean containsPattern(String line){
+        // Built-in String.matches(pattern) to see if line matches given regex.
+        return line.matches(this.regex);
+    }
+
+    //
+    @Override
+    void writeToFile(List<String> lines) throws IOException{
+        Path file = (new File(outFile)).toPath();
+        try{
+            // NOTE: creates a new, empty file named by abstract pathname only if a file
+            //       with this name does not already exist.
+            Files.createFile(file);
+        } catch (IOException e) {
+            // NOTE: changed from ignore because something should be done even if
+            //       the file wasn't created.
+            e.printStackTrace();
+        }
+        Files.write(file, lines);
+    }
+
+    @Override
+    public String getRootPath() {
+        return rootPath;
+    }
+    @Override
+    public void setRootPath(String rootPath) {
+        this.rootPath = rootPath;
+    }
+    @Override
+    public String getRegex() {
+        return regex;
+    }
+    @Override
+    public void setRegex(String regex) {
+        this.regex = regex;
+    }
+    @Override
+    public String getOutFile() {
+        return outFile;
+    }
+    @Override
+    public void setOutFile(String outFile) {
+        this.outFile = outFile;
+    }
+}
