a9085a80ecc24acc4f4c404fa9288d16cc28b8c8
Anne Nguyen <contact@anne-nguyen.io>
2020-02-08 15:56:18 -0500

    Quote model.

---
+++ springboot/src/main/java/ca/jrvs/apps/trading/model/domain/Quote.java
@@ -0,0 +1,21 @@
+package ca.jrvs.apps.trading.model.domain;
+
+public class Quote implements Entity<String>{
+
+    private String ticker;
+    private Double lastPrice;
+    private Double bidPrice;
+    private Integer bidSize;
+    private Double askPrice;
+    private Integer askSize;
+
+    @Override
+    public String getId(){
+        return ticker;
+    }
+
+    @Override
+    public void setId(String id) {
+        this.ticker = id;
+    }
+}
