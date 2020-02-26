fc75de357a9688cacfd53a26fd977d5dfa6a6d83
Anne Nguyen <contact@anne-nguyen.io>
2020-02-26 11:06:34 -0500

    Modified test configuration.

--- springboot/src/test/java/ca/jrvs/apps/trading/TestConfig.java
+++ springboot/src/test/java/ca/jrvs/apps/trading/TestConfig.java
@@ -1,11 +1,26 @@
 package ca.jrvs.apps.trading;
 
+import ca.jrvs.apps.trading.model.config.MarketDataConfig;
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.http.conn.HttpClientConnectionManager;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
+@Configuration
+@ComponentScan(basePackages = {"ca.jrvs.trading.dao", "ca.jrvs.apps.trading.service"})
 public class TestConfig {
+
+    @Bean
+    public MarketDataConfig marketDataConfig(){
+        MarketDataConfig marketDataConfig = new MarketDataConfig();
+        marketDataConfig.setHost("https://cloud.iexapis.com/v1/");
+        marketDataConfig.setToken(System.getenv("IEX_PUB_TOKEN"));
+        return marketDataConfig;
+    }
 
     @Bean
     public DataSource dataSource(){
@@ -18,5 +33,13 @@ public class TestConfig {
         basicDataSource.setUsername(user);
         basicDataSource.setPassword(password);
         return basicDataSource;
+    }
+
+    @Bean
+    public HttpClientConnectionManager httpClientConnectionManager(){
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(50);
+        connectionManager.setDefaultMaxPerRoute(50);
+        return connectionManager;
     }
 }
