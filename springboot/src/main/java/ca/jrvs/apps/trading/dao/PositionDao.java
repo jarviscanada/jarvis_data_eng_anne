7abd1891e54f632b0b72497eedff0ee3d71741d6
Anne Nguyen <contact@anne-nguyen.io>
2020-03-19 10:03:17 -0400

    Created Position model and its DAO.

---
+++ springboot/src/main/java/ca/jrvs/apps/trading/dao/PositionDao.java
@@ -0,0 +1,72 @@
+package ca.jrvs.apps.trading.dao;
+
+import ca.jrvs.apps.trading.model.domain.Position;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+
+import javax.sql.DataSource;
+
+public class PositionDao extends JdbcCrudDao<Position>  {
+
+    private static final Logger logger = LoggerFactory.getLogger(PositionDao.class);
+
+    private final String TABLE_NAME = "position";
+    private final String ID_COLUMN = "accountID";
+
+    private JdbcTemplate jdbcTemplate;
+    private SimpleJdbcInsert simpleJdbcInsert;
+
+    @Autowired
+    public PositionDao(DataSource dataSource){
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+        this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource).withTableName(TABLE_NAME).usingGeneratedKeyColumns(ID_COLUMN);
+    }
+
+    @Override
+    public JdbcTemplate getJdbcTemplate() {
+        return this.jdbcTemplate;
+    }
+
+    @Override
+    public SimpleJdbcInsert getSimpleJdbcInsert() {
+        return this.simpleJdbcInsert;
+    }
+
+    @Override
+    public String getTableName() {
+        return TABLE_NAME;
+    }
+
+    @Override
+    public String getIdColumnName() {
+        return ID_COLUMN;
+    }
+
+    @Override
+    Class<Position> getEntityClass() {
+        return Position.class;
+    }
+
+    @Override
+    public int updateOne(Position entity) {
+        throw new UnsupportedOperationException("Not implemented.");
+    }
+
+    @Override
+    public <S extends Position> Iterable<S> saveAll(Iterable<S> iterable) {
+        throw new UnsupportedOperationException("Not implemented.");
+    }
+
+    @Override
+    public void delete(Position position) {
+        throw new UnsupportedOperationException("Not implemented.");
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends Position> iterable) {
+        throw new UnsupportedOperationException("Not implemented.");
+    }
+}
