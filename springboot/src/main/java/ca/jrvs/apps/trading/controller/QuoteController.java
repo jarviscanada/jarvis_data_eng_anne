f02dfd060112bb9f704b3e948953968b7fc9b4ca
Anne Nguyen <contact@anne-nguyen.io>
2020-04-17 09:08:31 -0400

    Added API annotations.

--- springboot/src/main/java/ca/jrvs/apps/trading/controller/QuoteController.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/controller/QuoteController.java
@@ -3,13 +3,21 @@ package ca.jrvs.apps.trading.controller;
 import ca.jrvs.apps.trading.model.domain.IexQuote;
 import ca.jrvs.apps.trading.model.domain.Quote;
 import ca.jrvs.apps.trading.service.QuoteService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(
+        value = "quote",
+        produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 @Controller
 @RequestMapping("/quote")
 public class QuoteController {
@@ -21,8 +29,11 @@ public class QuoteController {
         this.quoteService = quoteService;
     }
 
-    // @ApiOperation(value = "Show iexQuote", notes = "Show iexQuote for given ticker.")
-    // @ApiOperation(value = {@ApiResponse(code = 404, message = "Ticker is not found.")
+    @ApiOperation(
+            value = "Show iexQuote",
+            notes = "Show iexQuote for given ticker.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 404, message = "Ticker is not found.")})
     @GetMapping(path = "/iex/ticker/{ticker}")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
@@ -34,6 +45,9 @@ public class QuoteController {
         }
     }
 
+    @ApiOperation(
+            value = "Update quote table using IEX data.",
+            notes = "Update all quotes in the quote table. Use IEX trading API as market data source.")
     @PutMapping(path = "/iexMarketData")
     @ResponseStatus(HttpStatus.OK)
     public void updateMarketData(){
@@ -44,6 +58,9 @@ public class QuoteController {
         }
     }
 
+    @ApiOperation(
+            value = "Update a given quote in the quote table.",
+            notes = "Manually updates a quote in the quote table using IEX market data.")
     @PutMapping(path = "/")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
@@ -55,6 +72,12 @@ public class QuoteController {
         }
     }
 
+    @ApiOperation(
+            value = "Add a new ticker to the dailyList (quote table).",
+            notes = "Add a new ticker/symbol to the quote table so trader can trade this securely.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 404, message = "Ticker is not found in IEX system.")
+    })
     @PostMapping(path = "/tickerId/{tickerId}")
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
@@ -66,6 +89,10 @@ public class QuoteController {
         }
     }
 
+    @ApiOperation(
+            value = "Show the dailyList (quote table).",
+            notes = "Show dailyList for this trading system."
+    )
     @GetMapping(path = "/dailyList")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
